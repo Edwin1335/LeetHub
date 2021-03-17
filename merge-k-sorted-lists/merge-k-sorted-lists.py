d827eafc8a15eba2cd2befff321f74be9d61bed3
Kit_Cat <39327521+Edwin1335@users.noreply.github.com>
2021-03-17 00:20:23 -0500

    Time: 168 ms (24.90%), Space: 18.1 MB (48.84%) - LeetHub

--- merge-k-sorted-lists/merge-k-sorted-lists.py
+++ merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -2,35 +2,44 @@
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
-#         self.next = next
+#         self.next = next 
+#   PQ  [()(C2, 1)(C1, 2)]
+#   retriveNode = PQ.get()-> C3
+#   [C2]->(1)->(2)->(4)
+#   [C1]->(2)->(6)
+#   [C3]->(4)->(5)
+                     
+#   [Trav]->[C3]->(1)->(4)->(5)
+#   Head("Dummy")->
+#   Trav = Head
+#   Trav.next = retriveNode
+#   
 
 from queue import PriorityQueue
 class Solution:
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
-        k = len(lists)
-        q = PriorityQueue(maxsize=k)
-        dummy = ListNode()
-        current = dummy
+        que = PriorityQueue()
+        sorted_list = ListNode("Dummy")
+        trav = sorted_list
         
-        # Insert heads into queue
-        for index, node in enumerate(lists):
-            if node:
-                q.put((node.val, index, node))
-        while not q.empty():
-            popped = q.get()
-            current.next, index = popped[2], popped[1]
-            current = current.next
-            if current.next:
-                q.put((current.next.val, index, current.next))
-                
-        return dummy.next
+        # Insert into queue in ascending order Time: O(nLogn)
+        for index, lst in enumerate(lists):
+            if lst:
+                que.put((lst.val, index, lst))
 
-    
-
-# Queue:
-#                   C
-# dummy-> 1 -> 1 -> 2-> 6 
-#     
-# 
-# 3-> 4
-# 4 -> 5    
+        # Loop until queue is empty Time: O(n)
+        while not que.empty():
+            q_value = que.get()
+            index, temp = q_value[1], q_value[2]
+            trav.next = temp
+            trav = trav.next
+            if trav.next:
+                que.put((trav.next.val, index, trav.next))
+            
+        # Return sorted list
+        return sorted_list.next
+        
+        
+            
+        
+        
